2b3da4199fe5fee04f5706ac924eb802fc78d651
Anirudh Konidala <kidskodingclub@gmail.com>
2025-02-23 03:15:45 -0600

    initial commit

---
+++ file-reader/src/main.rs
@@ -0,0 +1,32 @@
+use std::env;
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    let file_path = format!("files/{}.txt", args[1]);
+    let file = File::open(file_path);
+    let file = match file {
+        Ok(file) => file,
+        Err(error) => {
+            match error.kind() {
+                std::io::ErrorKind::NotFound => {
+                    panic!("File not found: {}", error)
+                }
+                _ => {
+                    panic!("Error opening file: {}", error)
+                }
+            }
+        }
+    };
+
+    let reader = BufReader::new(file);
+    for line in reader.lines() {
+        match line {
+            Ok(line) => println!("{}", line),
+            Err(error) => {
+                panic!("Error reading line: {}", error)
+            }
+        }
+    }
+}
